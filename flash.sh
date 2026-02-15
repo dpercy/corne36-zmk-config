e20b1376a72c34276188f29eed44adde2f7bde1a
David Percy <david@dpercy.name>
2026-02-15 16:01:48 -0500

    make script less flaky

--- flash.sh
+++ flash.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+wait_until_sda_present() {
+    while ! lsblk | grep -q sda
+    do
+        sleep 0.5
+    done
+}
+
+wait_until_sda_absent() {
+    while lsblk | grep -q sda
+    do
+        sleep 0.5
+    done
+}
+
 flash_side() {
     side=$1
 
-    watch -g  -n 0.5  lsblk
+    lsblk
+    wait_until_sda_present
     sudo mount /dev/sda /mnt/
     lsblk
     sudo cp firmware/corne_$side\ nice_view_adapter\ nice_view-nice_nano_v2-zmk.uf2 /mnt
     lsblk
     sudo umount /mnt
+    wait_until_sda_absent
     lsblk
 }
 
