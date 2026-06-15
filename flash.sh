7ad4dd03fed5ec19aa461a2c9451bd88f6c06a43
David Percy <david@dpercy.name>
2026-06-14 23:31:37 -0400

    fix flash; fix syntax

--- flash.sh
+++ flash.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-wait_until_sda_present() {
-    while ! lsblk | grep -q sda
-    do
-        sleep 0.5
-    done
-}
+disk=/dev/disk/by-label/NICENANO
 
-wait_until_sda_absent() {
-    while lsblk | grep -q sda
+wait_until_disk_present() (
+    set +x
+    while [ ! -b $disk ]
     do
         sleep 0.5
     done
-}
+)
+
+wait_until_disk_absent() (
+    set +x
+    while [ -b $disk ]
+    do
+        sleep 0.5
+    done
+)
 
 flash_side() {
     side=$1
 
     lsblk
-    wait_until_sda_present
-    sudo mount /dev/sda /mnt/
+    wait_until_disk_present
+    sudo mount $disk /mnt/
     lsblk
     sudo cp firmware/corne_$side\ nice_view_adapter\ nice_view-nice_nano_v2-zmk.uf2 /mnt
     lsblk
     sudo umount /mnt
-    wait_until_sda_absent
+    wait_until_disk_absent
     lsblk
 }
 
